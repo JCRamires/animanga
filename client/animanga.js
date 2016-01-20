3ac4f28d33132eb2e7e97cb8fea600931f580637
Juan Carlos Ramires <jcramires851@gmail.com>
2016-01-20 13:08:38 -0200

    changing my mind over semicolons

--- client/animanga.js
+++ client/animanga.js
@@ -1,42 +1,42 @@
-Works = new Mongo.Collection('works')
+Works = new Mongo.Collection('works');
 
-Meteor.subscribe('allGenres')
-Genres = new Mongo.Collection('genres')
+Meteor.subscribe('allGenres');
+Genres = new Mongo.Collection('genres');
 
-Meteor.subscribe('allThemes')
-Themes = new Mongo.Collection('themes')
+Meteor.subscribe('allThemes');
+Themes = new Mongo.Collection('themes');
 
 
-let filteredWorksSubscribeHandle
-const NUMBER_OF_RECORDS_TO_FETCH = 20
-const $window = $(window)
+let filteredWorksSubscribeHandle;
+const NUMBER_OF_RECORDS_TO_FETCH = 20;
+const $window = $(window);
 
 Template.body.helpers({
     works(){
-        return Works.find({}, {sort: {name: 1}})
+        return Works.find({}, {sort: {name: 1}});
     }
-})
+});
 
 Template.body.helpers({
     genres(){
-        return Genres.find({}, {sort: {name: 1}})
+        return Genres.find({}, {sort: {name: 1}});
     },
     themes(){
-        return Themes.find({}, {sort: {name: 1}})
+        return Themes.find({}, {sort: {name: 1}});
     }
-})
+});
 
 Template.work.events({
     'click .workCard': function (event) {
-        event.preventDefault()
+        event.preventDefault();
     }
-})
+});
 
 Template.menuNavbar.events({
     'change #filters': function () {
-        Session.set('filters', $('#filters').serializeJSON())
+        Session.set('filters', $('#filters').serializeJSON());
     }
-})
+});
 
 Template.menuNavbar.onRendered(function () {
     $('#genreSelect').selectize({
@@ -44,10 +44,10 @@ Template.menuNavbar.onRendered(function () {
         labelField: 'name',
         searchField: 'name',
         load(query, callback){
-            let genres = Genres.find({})
-            callback(genres.fetch())
+            let genres = Genres.find({});
+            callback(genres.fetch());
         }
-    })
+    });
 
     $('#themeSelect').selectize({
         valueField: 'name',
@@ -57,40 +57,39 @@ Template.menuNavbar.onRendered(function () {
             return {
                 value: input,
                 text: input
-            }
+            };
         },
         load(query, callback){
-            let themes = Themes.find({})
-            callback(themes.fetch())
+            let themes = Themes.find({});
+            callback(themes.fetch());
         }
-    })
-})
+    });
+});
 
 Tracker.autorun(function () {
-    const filters = Session.get('filters')
-    filteredWorksSubscribeHandle = Meteor.subscribeWithPagination('filteredWorks', filters, NUMBER_OF_RECORDS_TO_FETCH)
-
-})
+    const filters = Session.get('filters');
+    filteredWorksSubscribeHandle = Meteor.subscribeWithPagination('filteredWorks', filters, NUMBER_OF_RECORDS_TO_FETCH);
+});
 
 function loadMore() {
-    let threshold, target = $('#showMoreResults')
+    let threshold, target = $('#showMoreResults');
     if (!target.length){
-        return
+        return;
     }
 
-    threshold = $window.scrollTop() + $window.height() - target.height()
+    threshold = $window.scrollTop() + $window.height() - target.height();
 
     if (target.offset().top < threshold) {
         if (!target.data('visible')) {
-            target.data('visible', true)
-            filteredWorksSubscribeHandle.loadNextPage()
+            target.data('visible', true);
+            filteredWorksSubscribeHandle.loadNextPage();
         }
     } else {
         if (target.data('visible')) {
-            target.data('visible', false)
+            target.data('visible', false);
         }
     }
 
 }
 
-$window.scroll(loadMore)
+$window.scroll(loadMore);
--- server/animanga-server.js
+++ server/animanga-server.js
@@ -1,63 +1,63 @@
-Works = new Mongo.Collection('works')
-Genres = new Mongo.Collection('genres')
-Genres._ensureIndex({name: 1}, {unique: 1})
-Types = new Mongo.Collection('types')
-Types._ensureIndex({name: 1}, {unique: 1})
-Themes = new Mongo.Collection('themes')
-Themes._ensureIndex({name: 1}, {unique: 1})
+Works = new Mongo.Collection('works');
+Genres = new Mongo.Collection('genres');
+Genres._ensureIndex({name: 1}, {unique: 1});
+Types = new Mongo.Collection('types');
+Types._ensureIndex({name: 1}, {unique: 1});
+Themes = new Mongo.Collection('themes');
+Themes._ensureIndex({name: 1}, {unique: 1});
 
-const NUMBER_OF_WORKS_TO_FETCH_API = 50
+const NUMBER_OF_WORKS_TO_FETCH_API = 50;
 
 Meteor.startup(function () {
     if (Works.find().count() === 0) {
-        Meteor.call('initializeDB')
+        Meteor.call('initializeDB');
     }
-})
+});
 
 Meteor.methods({
     initializeDB(){
-        console.log('Fetching works')
+        console.log('Fetching works');
         const result = HTTP.get('http://www.animenewsnetwork.com/encyclopedia/reports.xml', {
             params: {
                 id: 155,
                 nlist: 200 //or 'all'
             }
-        })
+        });
 
-        let workIDs = []
-        const works = xml2js.parseStringSync(result.content)
+        let workIDs = [];
+        const works = xml2js.parseStringSync(result.content);
         works.report.item.forEach(function (entry) {
             addWork(entry);
-            workIDs.push(parseInt(entry.id))
-        })
+            workIDs.push(parseInt(entry.id));
+        });
 
-        console.log('Fetching work details')
-        workDetailsBatch(workIDs)
+        console.log('Fetching work details');
+        workDetailsBatch(workIDs);
     }
-})
+});
 
 function addWork(entry){
-    let type = entry.type.toString()
-    let workObj = {id: entry.id, gid: entry.gid, name: entry.name, type: entry.type, vintage: entry.vintage}
+    let type = entry.type.toString();
+    let workObj = {id: entry.id, gid: entry.gid, name: entry.name, type: entry.type, vintage: entry.vintage};
     switch (type) {
         case 'TV' || 'OAV' || 'ONA':
-            workObj.series = true
-            break
+            workObj.series = true;
+            break;
         case 'movie':
-            workObj.movie = true
-            break
+            workObj.movie = true;
+            break;
         case 'manga':
-            workObj.manga = true
-            break
+            workObj.manga = true;
+            break;
     }
 
-    Works.insert(workObj)
+    Works.insert(workObj);
 }
 
 function addWorkDetails(work){
     if(work.$ !== undefined){
-        addTypeFromWork(work)
-        addGenresAndThemesFromWork(work)
+        addTypeFromWork(work);
+        addGenresAndThemesFromWork(work);
     }
 }
 
@@ -65,184 +65,184 @@ function addTypeFromWork(work){
     if (Types.findOne({name: work.$.type}) === undefined) {
         Types.insert({
             name: work.$.type
-        })
+        });
     }
 }
 
 function addGenresAndThemesFromWork(work){
-    let workDetails = {workId: work.$.id, genres: [], themes: []}
-    let workInfo = work.info
+    let workDetails = {workId: work.$.id, genres: [], themes: []};
+    let workInfo = work.info;
 
     if (typeof workInfo === 'object') {
         Object.keys(workInfo).forEach(function (key) {
-            let workfInfoType = workInfo[key].$.type
-            let workInfoValue = workInfo[key]._
+            let workfInfoType = workInfo[key].$.type;
+            let workInfoValue = workInfo[key]._;
 
             switch (workfInfoType.toLowerCase()) {
                 case 'genres':
-                    workDetails.genres.push(workInfoValue)
+                    workDetails.genres.push(workInfoValue);
                     if (Genres.findOne({name: workInfoValue}) === undefined) {
                         Genres.insert({
                             name: workInfoValue
-                        })
+                        });
                     }
-                    break
+                    break;
                 case 'themes':
-                    workDetails.themes.push(workInfoValue)
+                    workDetails.themes.push(workInfoValue);
                     if (Themes.findOne({name: workInfoValue.toLowerCase()}) === undefined) {
                         Themes.insert({
                             name: workInfoValue.toLowerCase()
-                        })
+                        });
                     }
-                    break
+                    break;
                 case 'main title':
-                    workDetails.name = workInfoValue
-                    break
+                    workDetails.name = workInfoValue;
+                    break;
                 case 'plot summary':
-                    workDetails.plot = workInfoValue
-                    break
+                    workDetails.plot = workInfoValue;
+                    break;
                 case 'objectionable content':
                     if (workInfoValue.toLowerCase() === 'ma') {
-                        workDetails.mature = true
+                        workDetails.mature = true;
                     }
-                    break
+                    break;
                 case 'picture':
-                    let workImg = workInfo[key].img[1]
+                    let workImg = workInfo[key].img[1];
                     if (workImg !== undefined) {
-                        workDetails.picture = workImg.$
+                        workDetails.picture = workImg.$;
                     } else {
                         if (workInfo[key].img[0] !== undefined) {
-                            workDetails.picture = workInfo[key].img[0].$
+                            workDetails.picture = workInfo[key].img[0].$;
                         }
                     }
-                    break
+                    break;
                 case 'alternative title':
                     if (workInfo[key].$.lang.toLowerCase() === 'ja') {
-                        workDetails.alternativeTitle = workInfoValue
+                        workDetails.alternativeTitle = workInfoValue;
                     }
-                    break
+                    break;
             }
-        })
+        });
     }
 
     if (!_.has(workDetails, 'picture')) {
-        workDetails.picture = {src: '/default.jpg', temporary: true}
+        workDetails.picture = {src: '/default.jpg', temporary: true};
     }
 
-    persistWorkDetails(workDetails)
+    persistWorkDetails(workDetails);
 }
 
 function persistWorkDetails(work){
-    work.lastUpdate = new Date()
-    Works.update({id: work.workId}, {$set: {workDetails: work}})
+    work.lastUpdate = new Date();
+    Works.update({id: work.workId}, {$set: {workDetails: work}});
 }
 
 function workDetailsBatch(workIDs){
-    let batchIDs = []
+    let batchIDs = [];
     workIDs.forEach(function (id, index) {
         if (batchIDs.length < NUMBER_OF_WORKS_TO_FETCH_API) {
-            batchIDs.push(id)
+            batchIDs.push(id);
         }
         if (batchIDs.length === NUMBER_OF_WORKS_TO_FETCH_API || index + 1 === workIDs.length) {
-            let appendIDs
+            let appendIDs;
             batchIDs.forEach(function (id, index) {
                 if (index === 0) {
-                    appendIDs = id
+                    appendIDs = id;
                 } else {
-                    appendIDs += `/${id}`
+                    appendIDs += `/${id}`;
                 }
-            })
+            });
 
             const result = HTTP.get('http://cdn.animenewsnetwork.com/encyclopedia/api.xml', {
                 query: `title=${appendIDs}`
-            })
+            });
 
-            const works = xml2js.parseStringSync(result.content)
+            const works = xml2js.parseStringSync(result.content);
 
             Object.keys(works.ann).forEach(function (key) {
                 works.ann[key].forEach(function (work) {
-                    addWorkDetails(work)
-                })
-            })
+                    addWorkDetails(work);
+                });
+            });
 
-            console.log(`Fetched ${index+1} out of ${workIDs.length}`)
+            console.log(`Fetched ${index+1} out of ${workIDs.length}`);
 
-            batchIDs = []
+            batchIDs = [];
         }
-    })
+    });
 }
 
 function createWorkQueryObject(filters){
-    let queryObject = {}
-    let $and = []
+    let queryObject = {};
+    let $and = [];
 
     if (filters.types !== undefined && filters.types !== '') {
-        let types = {$in: []}
+        let types = {$in: []};
         filters.types.forEach(function (type) {
             switch (type) {
                 case 'series':
-                    types.$in.push('TV')
-                    types.$in.push('OAV')
-                    types.$in.push('ONA')
-                    break
+                    types.$in.push('TV');
+                    types.$in.push('OAV');
+                    types.$in.push('ONA');
+                    break;
                 case 'movies':
-                    types.$in.push('movie')
-                    break
+                    types.$in.push('movie');
+                    break;
                 case 'manga':
-                    types.$in.push(type)
-                    break
+                    types.$in.push(type);
+                    break;
             }
-        })
+        });
 
-        queryObject.type = types
+        queryObject.type = types;
     }
 
     if (filters.genres !== undefined && filters.genres !== '') {
         filters.genres.forEach(function (genre) {
-            let genreObj = {'workDetails.genres': genre}
-            $and.push(genreObj)
-        })
+            let genreObj = {'workDetails.genres': genre};
+            $and.push(genreObj);
+        });
     }
 
     if (filters.themes !== undefined && filters.themes !== '') {
         filters.themes.forEach(function (theme) {
-            let themeObj = {'workDetails.themes': theme}
-            $and.push(themeObj)
-        })
+            let themeObj = {'workDetails.themes': theme};
+            $and.push(themeObj);
+        });
     }
 
     if (_.size($and) !== 0) {
-        queryObject.$and = $and
+        queryObject.$and = $and;
     }
 
-    return queryObject
+    return queryObject;
 }
 
-Meteor.publish('allGenres', () => Genres.find({}, {sort: {name: 1}}))
+Meteor.publish('allGenres', () => Genres.find({}, {sort: {name: 1}}));
 
-Meteor.publish('allThemes', () => Themes.find({}, {sort: {name: 1}}))
+Meteor.publish('allThemes', () => Themes.find({}, {sort: {name: 1}}));
 
 Meteor.publish('searchThemes', query => {
-    const search = new RegExp('^' + query, 'i')
-    return Themes.find({name: search}).sort({name: 1})
-})
+    const search = new RegExp('^' + query, 'i');
+    return Themes.find({name: search}).sort({name: 1});
+});
 
 Meteor.publish('filteredWorks', (filters, limit) => {
     if (filters !== null) {
         if (filters.genres !== undefined && filters.genres !== '') {
-            filters.genres = filters.genres.split(',')
+            filters.genres = filters.genres.split(',');
         }
 
         if (filters.themes !== undefined && filters.themes !== '') {
-            filters.themes = filters.themes.split(',')
+            filters.themes = filters.themes.split(',');
         }
 
-        const queryObject = createWorkQueryObject(filters)
+        const queryObject = createWorkQueryObject(filters);
 
         if (_.isEmpty(queryObject)) {
-            return []
+            return [];
         }
 
-        return Works.find(queryObject, {sort: {name: 1}, limit: limit})
+        return Works.find(queryObject, {sort: {name: 1}, limit: limit});
     }
-})
+});
